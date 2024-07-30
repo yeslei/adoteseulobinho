f9145960d648d3cbd3fe322333ebd8757b443a2f
IcaroD <icaro.davila.santos@gmail.com>
2024-07-30 19:12:32 -0300

    fix: lista lobo

--- scripts/listalobo.js
+++ scripts/listalobo.js
@@ -1,65 +1,76 @@
-const content = document.querySelector(".content");
-const search = document.querySelector("input[type='search']");
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.querySelector(".content");
+    const search = document.querySelector("input[type='search']");
+    const checkbox = document.querySelector("#adotadosCheckbox");
 
-var items = [];
+    let items = [];
 
-function addHTML(lobinho, num) {
-    var strAdotar = "Adotar";
-    if (lobinho.adotado) {
-        strAdotar = "Adotado";
-    }
-
-    const div = document.createElement("div");
-    const reverseClass = num % 2 === 0 ? "lobo-reverse" : "";
-    div.innerHTML = `
-        <div class="lobo ${reverseClass}">
-            <div class="imagemLobo">
-                <img src="${lobinho.imagem}">
-            </div>
-            <div class="dadosDoLobo">
-                <div>
-                    <h1>${lobinho.nome}</h1>
-                    <a href="showlobinho.html?id=${lobinho.id}" class="botaoAdotar">${strAdotar}</a>
+    function addHTML(lobinho, num) {
+        var strAdotar = "Adotar";
+        var classAdotar = "botaoAdotar"
+        if (lobinho.adotado) {
+            strAdotar = "Adotado";
+            classAdotar = "botaoAdotado"
+        }
+        
+        const reverseClass = num % 2 === 0 ? "" : "lobo-reverse";
+        
+        const div = document.createElement("div");
+        div.innerHTML = `
+            <div class="lobo ${reverseClass}">
+                <div class="imagemLobo">
+                    <img src="${lobinho.imagem}">
                 </div>
-                <p>
-                    ${lobinho.descricao}
-                </p>
-            </div>
-        </div>`;
-    content.append(div);
-}
-
-
-const fetchConfig = {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
+                <div class="dadosDoLobo">
+                    <div>
+                        <h1>${lobinho.nome}</h1>
+                        <a href="showlobinho.html?id=${lobinho.id}" class="${classAdotar}">${strAdotar}</a>
+                    </div>
+                    <p>${lobinho.descricao}</p>
+                </div>
+            </div>`;
+        content.append(div);
     }
-};
 
-fetch("http://localhost:3000/lobinhos", fetchConfig)
-    .then((resposta) => resposta.json())
-    .then((lobinhos) => {
-        let index = 0;
-        lobinhos.forEach((lobinho) => {
-            if (!lobinho.adotado) {
+    const fetchConfig = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+
+    fetch("http://localhost:3000/lobinhos", fetchConfig)
+        .then((resposta) => {
+            if (!resposta.ok) {
+                throw new Error('Erro na resposta da rede');
+            }
+            return resposta.json();
+        })
+        .then((lobinhos) => {
+            lobinhos.forEach((lobinho, index) => {
                 addHTML(lobinho, index);
                 items.push(lobinho);
-                index++;
-            }
+            });
+        })
+        .catch((error) => {
+            console.error('Erro ao buscar lobinhos:', error);
         });
-    });
 
-search.oninput = () => {
-    content.innerHTML = "";
+    function updateContent() {
+        content.innerHTML = "";
+        let filteredItems = items;
 
-    let index = 0;
-    items
-        .filter((item) =>
-            item.nome.toLowerCase().includes(search.value.toLowerCase())
-        )
-        .forEach((item) => {
-            addHTML(item, index);
-            index++;
-        });
-};
+        if (checkbox.checked) {
+            filteredItems = items.filter(item => item.adotado);
+        } else {
+            filteredItems = items.filter(item => !item.adotado);
+        }
+
+        filteredItems
+            .filter(item => item.nome.toLowerCase().includes(search.value.toLowerCase()))
+            .forEach((item, index) => addHTML(item, index));
+    }
+
+    search.oninput = updateContent;
+    checkbox.addEventListener('change', updateContent);
+});
