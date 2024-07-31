3805890c6743cae7b12386a6421b64747e959004
Weslley Ribeiro <weslleysilvaribeiro35@gmail.com>
2024-07-30 22:50:45 -0300

    fix: correcao do bug do id que criava todos com id 1001

--- scripts/cadastrolobo.js
+++ scripts/cadastrolobo.js
@@ -1,43 +1,51 @@
-let id = 1000;
-
-function postLobo(){
+function postLobo() {
     let nome = document.querySelector("#nome_lobo").value;
     let idade = document.querySelector("#idade_lobo").value;
     let imagem = document.querySelector("#link_lobo").value;
     let descricao = document.querySelector("#descricao_lobo").value;
 
-    id += 1;
-
-    let fetchBody = {
-        "id": id.toString(),
-        "nome": nome,
-        "idade": idade,
-        "descricao": descricao,
-        "imagem": imagem,
-        "adotado": false,
-        "nomeDono": null,
-        "idadeDono": null,
-        "emailDono": null
-    }
-
-    const fetchConfig = {
-        "method": "POST",
-        "body": JSON.stringify(fetchBody),
-        "headers": { "Content-Type" : "application/json" }
-    }
-    
-    fetch("http://localhost:3000/lobinhos", fetchConfig)
+    fetch("http://localhost:3000/lobinhos")
         .then((resposta) => {
-            resposta.json()
-                .then((resposta)=>{
-                    console.log(resposta);
-                    alert("Lobo adicionado!")
-                })
-                .catch(error => console.log(error));
+            return resposta.json();
         })
-        .catch(error => console.log(error));
+        .then((lobinhos) => {
+        
+            let id = lobinhos.length + 1;
 
+            let fetchBody = {
+                "id": id.toString(),
+                "nome": nome,
+                "idade": idade,
+                "descricao": descricao,
+                "imagem": imagem,
+                "adotado": false,
+                "nomeDono": null,
+                "idadeDono": null,
+                "emailDono": null
+            }
+
+            const fetchConfig = {
+                "method": "POST",
+                "body": JSON.stringify(fetchBody),
+                "headers": { "Content-Type": "application/json" }
+            }
+
+            return fetch("http://localhost:3000/lobinhos", fetchConfig);
+        })
+        .then((resposta) => {
+            if (!resposta.ok) {
+                throw new Error('Erro na resposta da rede');
+            }
+            return resposta.json();
+        })
+        .then((resposta) => {
+            console.log(resposta);
+            alert("Lobo adicionado!");
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 }
 
 let botaoSalvar = document.querySelector(".save");
-botaoSalvar.addEventListener("click",postLobo);
+botaoSalvar.addEventListener("click", postLobo);
