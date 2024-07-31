91efe1e36d2969bfed95d3cb92e514487068882d
Eduardo Osborne <eosborne@id.uff.br>
2024-07-30 23:12:11 -0300

    feat: adicaod de alertas de validacao de dados em cadastro de novo lobo

--- scripts/cadastrolobo.js
+++ scripts/cadastrolobo.js
@@ -4,47 +4,61 @@ function postLobo() {
     let imagem = document.querySelector("#link_lobo").value;
     let descricao = document.querySelector("#descricao_lobo").value;
 
-    fetch("http://localhost:3000/lobinhos")
-        .then((resposta) => {
-            return resposta.json();
-        })
-        .then((lobinhos) => {
-        
-            let id = lobinhos.length + 1;
+    if (nome.length > 60 || nome.length < 4 || nome === ""){
+        alert("O nome do Lobo deve ter entre 4 e 60 caracteres")
+    }
+    else if (idade < 0 || idade > 100 || idade == NaN || idade == "") {
+        alert("O lobo deve ter entre 0 e 100 anos")
+    }
+    else if (imagem === "") {
+        alert("Você deve enviar um link de imagem.")
+    }
+    else if (descricao === "" || descricao.length < 10 || descricao.length > 255) {
+        alert("Você deve enviar uma descrição entre 10 e 255 caracteres.")
+    }
+    else {
+        fetch("http://localhost:3000/lobinhos")
+            .then((resposta) => {
+                return resposta.json();
+            })
+            .then((lobinhos) => {
+            
+                let id = lobinhos.length + 1;
 
-            let fetchBody = {
-                "id": id.toString(),
-                "nome": nome,
-                "idade": idade,
-                "descricao": descricao,
-                "imagem": imagem,
-                "adotado": false,
-                "nomeDono": null,
-                "idadeDono": null,
-                "emailDono": null
-            }
+                let fetchBody = {
+                    "id": id.toString(),
+                    "nome": nome,
+                    "idade": idade,
+                    "descricao": descricao,
+                    "imagem": imagem,
+                    "adotado": false,
+                    "nomeDono": null,
+                    "idadeDono": null,
+                    "emailDono": null
+                }
 
-            const fetchConfig = {
-                "method": "POST",
-                "body": JSON.stringify(fetchBody),
-                "headers": { "Content-Type": "application/json" }
-            }
+                const fetchConfig = {
+                    "method": "POST",
+                    "body": JSON.stringify(fetchBody),
+                    "headers": { "Content-Type": "application/json" }
+                }
 
-            return fetch("http://localhost:3000/lobinhos", fetchConfig);
-        })
-        .then((resposta) => {
-            if (!resposta.ok) {
-                throw new Error('Erro na resposta da rede');
-            }
-            return resposta.json();
-        })
-        .then((resposta) => {
-            console.log(resposta);
-            alert("Lobo adicionado!");
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+                return fetch("http://localhost:3000/lobinhos", fetchConfig);
+            })
+            .then((resposta) => {
+                if (!resposta.ok) {
+                    throw new Error('Erro na resposta da rede');
+                }
+                return resposta.json();
+            })
+            .then((resposta) => {
+                console.log(resposta);
+                alert("Lobo adicionado!");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 }
 
 let botaoSalvar = document.querySelector(".save");
